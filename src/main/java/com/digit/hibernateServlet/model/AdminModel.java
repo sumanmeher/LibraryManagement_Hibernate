43cb43a500a72bfaeaf001fa96cb0f27c7a33d37
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-08-09 13:03:02 +0530

    lsakd

--- src/main/java/com/digit/hibernateServlet/model/AdminModel.java
+++ src/main/java/com/digit/hibernateServlet/model/AdminModel.java
@@ -4,6 +4,31 @@ import java.sql.Connection;
 
 public class AdminModel {
 	public static Connection con;
+	
+	String admin_id;
+	String secret_pass;
+	
+	
+	public String getAdmin_id() {
+		return admin_id;
+	}
+
+
+	public void setAdmin_id(String admin_id) {
+		this.admin_id = admin_id;
+	}
+
+
+	public String getSecret_pass() {
+		return secret_pass;
+	}
+
+
+	public void setSecret_pass(String secret_pass) {
+		this.secret_pass = secret_pass;
+	}
+
+
 	public AdminModel() {
 		DatabaseModel db = new DatabaseModel();
 		con=DatabaseModel.con;
