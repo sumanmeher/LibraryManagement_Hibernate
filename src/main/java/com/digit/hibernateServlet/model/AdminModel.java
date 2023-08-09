1718536325f68d914ad8483f6ee15d0e3126b82d
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-08-09 14:00:45 +0530

    admin login

--- src/main/java/com/digit/hibernateServlet/model/AdminModel.java
+++ src/main/java/com/digit/hibernateServlet/model/AdminModel.java
@@ -1,36 +1,40 @@
 package com.digit.hibernateServlet.model;
 
-import java.sql.Connection;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import com.digit.hibernateServlet.bean.AdminDetails;
 
 public class AdminModel {
-	public static Connection con;
-	
-	String admin_id;
-	String secret_pass;
-	
-	
-	public String getAdmin_id() {
-		return admin_id;
-	}
-
-
-	public void setAdmin_id(String admin_id) {
-		this.admin_id = admin_id;
-	}
-
-
-	public String getSecret_pass() {
-		return secret_pass;
-	}
-
-
-	public void setSecret_pass(String secret_pass) {
-		this.secret_pass = secret_pass;
-	}
-
+	private Session session;
 
 	public AdminModel() {
-		DatabaseModel db = new DatabaseModel();
-		con=DatabaseModel.con;
+
+
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml"); // connects to cfg
+		ServiceRegistryBuilder builder = new ServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.buildServiceRegistry());
+
+		session = sessionFactory.openSession();
+		System.out.println("Connected to pf...");
+
 	}
+	
+	public boolean login(int username, String password) {
+
+		
+		Transaction tran=session.beginTransaction();
+		AdminDetails ad = (AdminDetails) session.get(AdminDetails.class,username);
+    	
+		if(ad.getSecret_pass().equals(password)) {
+			return true;
+		}
+		tran.commit();
+    	
+		return false;
+	}
+	
 }
