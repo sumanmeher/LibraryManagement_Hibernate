595821ea9be77b70d103fc717311c88c525c3b49
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-08-10 19:11:31 +0530

    changes in UI

--- src/main/java/com/digit/hibernateServlet/controller/Logout.java
+++ src/main/java/com/digit/hibernateServlet/controller/Logout.java
@@ -3,11 +3,13 @@ package com.digit.hibernateServlet.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/Logout")
 public class Logout extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
